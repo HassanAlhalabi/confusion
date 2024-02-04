2f54a2da19e199800dfdafc4956b1f9548983d20
Hassan Alhalabi <h.halabi@osoustech.com>
2024-02-04 15:25:38 +0300

    Add Auth

---
+++ config.ts
@@ -0,0 +1,4 @@
+export const config = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl' : "mongodb://127.0.0.1:27017/confusion"
+}
--- src/authenticate/index.ts
+++ src/authenticate/index.ts
@@ -1,6 +1,34 @@
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
+import { sign } from 'jsonwebtoken'
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import passport from 'passport';
 
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+import { config } from '../../config';
+
+export const getToken = function(user) {
+    return sign(user, config.secretKey,
+        {expiresIn: 3600});
+};
+
+const jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+const secretOrKey = config.secretKey;
+
+export const jwtPassport = passport.use(new Strategy({
+    jwtFromRequest,
+    secretOrKey
+},
+    (jwt_payload, done) => {
+        console.log("JWT payload: ", jwt_payload);
+        User.findOne({_id: jwt_payload._id}, (err, user) => {
+            if (err) {
+                return done(err, false);
+            }
+            else if (user) {
+                return done(null, user);
+            }
+            else {
+                return done(null, false);
+            }
+        });
+    }));
+
+export const verifyUser = passport.authenticate('jwt', {session: false});
